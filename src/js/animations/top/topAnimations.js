57c20812a126b4e9ab0c3bc3d06bc8fcdf92a470
Krzysztof <kpiekarz1983@gmail.com>
2019-11-21 21:46:44 +0100

    Added top animations

--- src/js/animations/top/topAnimations.js
+++ src/js/animations/top/topAnimations.js
@@ -10,11 +10,29 @@ const topHeading = document.querySelector('.top__heading'),
   topParagraph = document.querySelector('.top__paragraph'),
   topButton = document.querySelector('.top__button');
 
+gsap.set(topButton, { transitionDuration: 0 });
+
 const headerTL = () => {
   const tl = gsap.timeline();
-  tl.to([topHeading, topParagraph], { duration: 1, x: 300 });
+  tl.from([topHeading, topParagraph, topButton], {
+    duration: 1,
+    y: 30,
+    opacity: 0,
+    ease: 'Power2.easeIn',
+    stagger: 0.4
+  }).from(
+    topHeadingAfter,
+    {
+      cssRule: { width: 0 },
+      duration: 1.25,
+      ease: 'Power0.easeNone'
+    },
+    '-=1'
+  );
 
   return tl;
 };
+
+// ----------------------------------------------
 
 export default headerTL;
